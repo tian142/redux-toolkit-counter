5888c7859505c46d1ac7696fc3f58a17df86c633
Tian <tian.qiu142@gmail.com>
2021-02-11 17:30:19 -0800

    completed increment and decrement functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import {increment, decrement, selectCount} from './counterSlice'
 
 
 function App() {
   const count = useSelector(selectCount)
+  const dispatch = useDispatch()
   return (
-    <h1>{count}</h1>
+    <div>
+      <h1>{count}</h1>
+      <button
+      onClick={() => dispatch(increment())}
+      >Increase</button>
 
+
+      <button
+      onClick={()=> dispatch(decrement())}
+      >decrease</button>
+    </div>
   )
 }
 
